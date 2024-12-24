f09ec1f2690b1c2911f4c16af38d5970f591131e
Gabriel G.B. <gabrielgarciabuey@gmail.com>
2024-12-24 15:46:11 +0000

    feat(Robot): Add rotation around point of reference on turn_forever

--- src/robot.py
+++ src/robot.py
@@ -22,8 +22,10 @@ class Robot:
         self.right_motor = LargeMotor(OUTPUT_D)
         self.wheel_diameter = wheel_diameter
         self.wheel_base = wheel_base
-        self.left_speed = base_speed * speed_correction
-        self.right_speed = base_speed / speed_correction
+        self.base_speed = base_speed
+        self.speed_correction = speed_correction
+        self.base_left_speed = base_speed * speed_correction
+        self.base_right_speed = base_speed / speed_correction
         self.pos = Vector(0, 0)
         self.look_at = Vector(0, 1)
         self.left_motor_dis = 0.0
@@ -45,14 +47,14 @@ class Robot:
         return self.distance_to_tachos(wheel_travel_distance)
 
     def reset_motors(self):
-        self.log.info("Motor L pos after movement: {}".format(self.left_motor.position))
-        self.log.info("Motor R pos after movement: {}".format(self.right_motor.position))
+        self.log.debug("Motor L pos after movement: {}".format(self.left_motor.position))
+        self.log.debug("Motor R pos after movement: {}".format(self.right_motor.position))
         self.left_motor.reset()
         self.right_motor.reset()
         self.left_motor_dis = 0
         self.right_motor_dis = 0
-        self.log.info("Motor L pos after reset: {}".format(self.left_motor.position))
-        self.log.info("Motor R pos after reset: {}".format(self.right_motor.position))
+        self.log.debug("Motor L pos after reset: {}".format(self.left_motor.position))
+        self.log.debug("Motor R pos after reset: {}".format(self.right_motor.position))
 
     def update_odometry(self):
         self.log.info("Updating odometry")
@@ -87,9 +89,9 @@ class Robot:
         self.log.info("||> MOVING {} {:.4f} cm".format("FORWARD" if distance > 0 else "BACKWARD", distance))
         degrees_to_turn = self.distance_to_tachos(distance)
         self.log.info("Target Tachos: {:.4f}".format(degrees_to_turn))
-        self.left_motor.on_for_degrees(degrees=degrees_to_turn, speed=SpeedPercent(self.left_speed), brake=True,
+        self.left_motor.on_for_degrees(degrees=degrees_to_turn, speed=SpeedPercent(self.base_left_speed), brake=True,
                                        block=False)
-        self.right_motor.on_for_degrees(degrees=degrees_to_turn, speed=SpeedPercent(self.right_speed), brake=True,
+        self.right_motor.on_for_degrees(degrees=degrees_to_turn, speed=SpeedPercent(self.base_right_speed), brake=True,
                                         block=True)
         self.update_odometry()
         self.reset_motors()
@@ -99,9 +101,9 @@ class Robot:
         self.log.info("||> ROTATING {:.4f} deg...".format(degrees))
         wheel_degrees = self.rotation_to_wheel_degrees(degrees)
         self.log.info("Target Tachos: {:.4f}".format(wheel_degrees))
-        self.left_motor.on_for_degrees(degrees=-wheel_degrees, speed=SpeedPercent(self.left_speed), brake=True,
+        self.left_motor.on_for_degrees(degrees=-wheel_degrees, speed=SpeedPercent(self.base_left_speed), brake=True,
                                        block=False)
-        self.right_motor.on_for_degrees(degrees=wheel_degrees, speed=SpeedPercent(self.right_speed), brake=True,
+        self.right_motor.on_for_degrees(degrees=wheel_degrees, speed=SpeedPercent(self.base_right_speed), brake=True,
                                         block=True)
         self.update_odometry()
         self.reset_motors()
@@ -109,24 +111,40 @@ class Robot:
 
     def run_forever(self):
         self.log.info("||> MOVING IN A STRAIGHT LINE...")
-        self.left_motor.speed_sp = int(self.left_speed / 100 * self.left_motor.max_speed)
-        self.right_motor.speed_sp = int(self.right_speed / 100 * self.right_motor.max_speed)
+        self.left_motor.speed_sp = int(self.base_left_speed / 100 * self.left_motor.max_speed)
+        self.right_motor.speed_sp = int(self.base_right_speed / 100 * self.right_motor.max_speed)
         self.left_motor.run_forever()
         self.right_motor.run_forever()
 
-    def turn_forever(self, center_of_rotation=None, clockwise: bool = False):
+    def turn_forever(self, center_of_rotation: Vector = None, clockwise: bool = False):
         self.log.info(
-            "||> ROTATING {}...".format("AROUND {}".format(center_of_rotation) if center_of_rotation else "IN PLACE"))
-        self.left_motor.speed_sp = int(self.left_speed / 100 * self.left_motor.max_speed)
-        self.right_motor.speed_sp = int(self.right_speed / 100 * self.right_motor.max_speed)
-        self.left_motor.speed_sp = self.left_motor.speed_sp if clockwise else -self.left_motor.speed_sp
-        self.right_motor.speed_sp = -self.right_motor.speed_sp if clockwise else self.right_motor.speed_sp
+            "||> ROTATING {}...".format("AROUND {}".format(center_of_rotation) if center_of_rotation else "IN PLACE")
+        )
+        if center_of_rotation:
+            distance_to_center = len(center_of_rotation - self.pos)
+            robot_cor_angle = math.degrees(
+                math.acos(center_of_rotation.dot(self.look_at) / len(center_of_rotation) * len(self.look_at))
+            )
+            if robot_cor_angle != 90:
+                self.turn_degrees(90 - robot_cor_angle)
+            left_radius = distance_to_center - (self.wheel_base / 2)
+            right_radius = distance_to_center + (self.wheel_base / 2)
+            speed_ratio = right_radius / left_radius
+            self.left_motor.speed_sp = int(self.base_speed * self.speed_correction)
+            self.right_motor.speed_sp = int(self.base_speed * speed_ratio / self.speed_correction)
+            self.left_motor.speed_sp = self.left_motor.speed_sp if clockwise else -self.left_motor.speed_sp
+            self.right_motor.speed_sp = -self.right_motor.speed_sp if clockwise else self.right_motor.speed_sp
+        else:
+            self.left_motor.speed_sp = int(self.base_left_speed / 100 * self.left_motor.max_speed)
+            self.right_motor.speed_sp = int(self.base_right_speed / 100 * self.right_motor.max_speed)
+            self.left_motor.speed_sp = self.left_motor.speed_sp if clockwise else -self.left_motor.speed_sp
+            self.right_motor.speed_sp = -self.right_motor.speed_sp if clockwise else self.right_motor.speed_sp
         self.left_motor.run_forever()
         self.right_motor.run_forever()
 
     def stop(self):
         self.log.info("\n")
-        self.log.info("||> STOP MOTORS".format(SpeedPercent(self.left_speed)))
+        self.log.info("||> STOP MOTORS".format(SpeedPercent(self.base_left_speed)))
         self.left_motor.stop()
         self.right_motor.stop()
         self.update_odometry()
